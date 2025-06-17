cbc582cfb17492f7c51572ac768a3edcf5eca6ef
Hargun Sandhu <imhargunsandhu@gmail.com>
2025-06-17 08:58:57 +0530

    selector made

--- src/components/TaskSelector.tsx
+++ src/components/TaskSelector.tsx
@@ -1,18 +1,49 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const TaskSelector = () => {
-  const [selected, setSelected] = useState<'daily' | 'prioirty'>('daily');
+  const [selected, setSelected] = useState<'daily' | 'priority'>('daily');
+
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={() => setSelected('daily')}>
-        <Text>Daily Tasks</Text>
-        {selected == 'daily' && <View></View>}
+        <View style={styles.option}>
+          <Text
+            style={[styles.text, selected === 'daily' && styles.selectedText]}
+          >
+            Daily Tasks
+          </Text>
+          {selected === 'daily' && (
+            <LinearGradient
+              colors={['#667EEA', '#764BA2']}
+              style={styles.gradientLine}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+            />
+          )}
+        </View>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => setSelected('prioirty')}>
-        <Text>Priority Tasks</Text>
-        {selected == 'prioirty' && <View></View>}
+      <TouchableOpacity onPress={() => setSelected('priority')}>
+        <View style={styles.option}>
+          <Text
+            style={[
+              styles.text,
+              selected === 'priority' && styles.selectedText,
+            ]}
+          >
+            Priority Tasks
+          </Text>
+          {selected === 'priority' && (
+            <LinearGradient
+              colors={['#667EEA', '#764BA2']}
+              style={styles.gradientLine}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+            />
+          )}
+        </View>
       </TouchableOpacity>
     </View>
   );
@@ -24,8 +55,25 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingHorizontal: 20,
     marginTop: 30,
+    width: '95%',
   },
-  gradientLine: {},
+  option: {
+    alignItems: 'center',
+  },
+  gradientLine: {
+    height: 8,
+    width: 84,
+    borderRadius: 18,
+    marginTop: 4,
+  },
+  text: {
+    color: '#4C4B50',
+    fontSize: 26,
+    fontWeight: 'bold',
+  },
+  selectedText: {
+    color: '#FFF',
+  },
 });
 
 export default TaskSelector;
